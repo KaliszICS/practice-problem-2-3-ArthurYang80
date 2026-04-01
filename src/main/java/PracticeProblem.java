d6e26985f108b505fb42e154ddca9a58ba94f524
ArthurYang80 <342802865@tdsb.ca>
2026-04-01 02:38:06 +0000

    Finalized

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,8 +1,28 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-
+	
 	}
+
+	public static void bubbleSortString(String[] strings) {
+    boolean swapped = false;
+    
+    for (int j = 0; j < strings.length - 1; j++) {
+        swapped = false;
+        for (int i = 0; i < strings.length - 1 - j; i++) {
+            if (strings[i].compareToIgnoreCase(strings[i + 1]) > 0) {
+                String temp = strings[i];
+                strings[i] = strings[i + 1];
+                strings[i + 1] = temp;
+                swapped = true;
+            }
+        }
+      
+        if (!swapped) {
+            j = strings.length; 
+        }
+    }
+}
 
 	public static void q1() {
 		//Write question 1 code here
